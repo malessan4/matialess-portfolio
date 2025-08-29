f2674a570eb2aa913f913495ece09900d6021789
malessan4 <matialess@gmail.com>
2025-08-28 23:45:37 -0300

    Se crea la pagina Contacto

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function ContactPage(){
+    return(
+        <main className="container mx-auto p-8">
+            <h1 className="text-3xl font-bold">Contacto</h1>
+            <p className="mt-4">Puedes contactarme a través de este formulario. </p>
+        </main>
+    );
+}
+
