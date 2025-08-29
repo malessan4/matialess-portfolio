bd7cfdcec53830b498f9e5a060895275163effbc
malessan4 <matialess@gmail.com>
2025-08-28 23:41:14 -0300

    Se crea la pagina Proyectos

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function ProjectsPage(){
+    return(
+        <main className="container mx-auto p-8">
+            <h1 className="text-3xl font-bold">Mis Proyectos</h1>
+            <p className="mt-4">Aquí es donde mostraré mi trabajo.</p>
+        </main>
+    );  
+}
