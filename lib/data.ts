f8e9bec77d31a9435562dd603a507422017805dc
malessan4 <matialess@gmail.com>
2025-08-29 00:08:47 -0300

    se crea el archivo data, aca tengo que ponerle los datos posta de cada proyecto

---
+++ lib/data.ts
@@ -0,0 +1,19 @@
+import { Project } from "./types";  
+
+export const projectsData: Project[] = [
+    {
+        title: "Proyecto 1",
+        description: "Descripción del proyecto 1",
+        tags: ["React", "TypeScript"]
+    },
+    {
+        title: "Proyecto 2",
+        description: "Descripción del proyecto 2",
+        tags: ["Next.js", "Tailwind CSS"]
+    },
+    {
+        title: "Proyecto 3",
+        description: "Descripción del proyecto 3",
+        tags: ["Node.js", "Express"]
+    }
+];
