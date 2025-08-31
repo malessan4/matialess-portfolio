381095d2a3a4354415b1949b798b388dc05cc76e
malessan4 <matialess@gmail.com>
2025-08-31 00:34:50 -0300

    Se completa los datos con trabajos reales

--- lib/data.ts
+++ lib/data.ts
@@ -1,19 +1,19 @@
-import { Project } from "./types";  
+import { Project } from "./types";
 
 export const projectsData: Project[] = [
     {
-        title: "Proyecto 1",
-        description: "Descripción del proyecto 1",
-        tags: ["React", "TypeScript"]
+        title: "Landing Page de Folkode",
+        description: "Construcción de una landing page para Folkode, un equipo de desarolladores de software. La página presenta información sobre los proyectos que armo el equipo, testimonios de clientes y un formulario de contacto.",
+        tags: ["React", "TypeScript", "Tailwind CSS", "Next.js"]
     },
     {
-        title: "Proyecto 2",
-        description: "Descripción del proyecto 2",
-        tags: ["Next.js", "Tailwind CSS"]
+        title: "Mediapp",
+        description: "Creacion de una aplicación web para la gestión de historias médicas. La aplicación permite a un usuario autorizado a crear historias clinicas, chequear estudios y consultas anteriores. Cumpliendo con las normativas de seguridad y privacidad de datos.",
+        tags: ["Go", "React", "TypeScript", "Tailwind CSS"]
     },
     {
-        title: "Proyecto 3",
-        description: "Descripción del proyecto 3",
-        tags: ["Node.js", "Express"]
+        title: "RadioGo",
+        description: "Pagina web para una radio online. La página permite a los usuarios escuchar la transmisión en vivo, ver el horario de programas y contactar a la estación.",
+        tags: ["React", "TypeScript", "Tailwind CSS", "Next.js"]
     }
 ];
