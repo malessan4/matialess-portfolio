778947dc1841c8b260a8e61ae8129d4eda09218c
malessan4 <matialess@gmail.com>
2026-01-12 13:03:40 -0300

    cambio en tecnologias usadas en el proyecto

--- lib/data.ts
+++ lib/data.ts
@@ -11,7 +11,7 @@ export const projectsData: Project[] = [
   {
     title: "Consul-medic: Sistema de Gestión Médica",
     description: "Desarrollo de una plataforma Full Stack para la digitalización de consultorios médicos. El sistema gestiona historias clínicas, adjunta estudios médicos (imágenes/PDFs) mediante almacenamiento de objetos y genera recetas digitales listas para imprimir. Incluye un dashboard interactivo para métricas de pacientes.",
-    tags: ["Next.js", "NestJS", "TypeScript", "PostgreSQL", "Prisma", "Docker", "Tailwind CSS"],
+    tags: ["Next.js", "Nest.js", "TypeScript", "PostgreSQL", "Supabase", "Prisma", "Docker", "Tailwind CSS"],
     imageUrl: "/images/consul-medic1.jpeg",
     link: "https://consul-medic.vercel.app/login"
   },
